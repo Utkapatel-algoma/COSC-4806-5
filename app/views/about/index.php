6079d36eb5659e9d20b2264856fd507d2d47eb5b
Utkapatel-algoma <Bittupatel3312@gmail.com>
2025-07-18 15:13:35 +0000

    Checked and confirmed - All links on the site are working and taking user to the destinations.

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,7 +1,7 @@
 <?php require_once VIEWS . DS . 'templates/header_public.php'; ?>
 
     <h2>About Our Application</h2>
-    <p>This is a simple PHP MVC application demonstrating core functionalities like user authentication and reminder management.</p>
+    <p>This is a simple PHP MVC application demonstrating core functionalities like User authentication and reminder management.</p>
     <p>It's part of a COSC 4806 assignment.</p>
     <p><a href="/home">Back to Home</a></p>
 
